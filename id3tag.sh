902b6414a5801739168f0fb1dcdcd8882583374e
DarkCat09 <a.chechkenev@yandex.ru>
2023-02-03 12:06:41 +0400

    Tagging, 755 mode

---
+++ id3tag.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+ask () {
+    read -r -p "[Y/n] " answer
+    if [[ $answer =~ [NnНн] ]]; then
+        echo 0
+    else
+        echo 1
+    fi
+}
+
+echo "Enter directory where your music is located:"
+read -e -r -i "./convert" directory
+directory="${directory:-./convert}"
+
+echo
+echo "Copy files into ./tagged/artist/album directory?"
+copy_arg=$(ask)
+echo
+
+find "$directory" -type f -name "*.mp3" -exec \
+bash ./.id3tag_helper.sh "$copy_arg" {} \;
